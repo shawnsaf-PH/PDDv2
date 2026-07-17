2b538dff789af2bdeefc0bfaca49daf931a81354
shawnsaf-PH <304648310+shawnsaf-PH@users.noreply.github.com>
2026-07-17 11:56:21 -0700

    Add Get-PlatformConfiguration function

--- src/Configuration/PlatformConfigurationService.ps1
+++ src/Configuration/PlatformConfigurationService.ps1
@@ -1,1 +1,20 @@
+using module ..\Models\PlatformConfiguration.ps1
 
+function Get-PlatformConfiguration {
+
+    $config = :new()
+
+    $config.PddDirectory = "\\USDBTLBCA1MS1IT\pdd$\PDD"
+    $config.ConfigDirectory = "\\USDBTLBCA1MS1IT\pdd$\Config"
+    $config.LogDirectory = "\\USDBTLBCA1MS1IT\pdd$\LogFiles"
+    $config.IniFilesDirectory = "\\USDBTLBCA1MS1IT\pdd$\IniFiles"
+
+    $config.Toolboxes["BASIC"] = "\\USDBTLBCA1MS1IT\Toolbox$\Basic_Toolbox"
+    $config.Toolboxes["OPTION"] = "\\USDBTLBCA1MS1IT\Toolbox$\Option_Toolbox"
+    $config.Toolboxes["SITE"] = "\\USDBTLBCA1MS1IT\Toolbox$\Site_Toolbox"
+
+    $config.Catalogs += "Win11.ini"
+    $config.Catalogs += "Site.ini"
+
+    return $config
+}
