8d7df44c43318b2bf88744523f0a7d14807d55eb
shawnsaf-PH <304648310+shawnsaf-PH@users.noreply.github.com>
2026-07-17 11:57:00 -0700

    Add PlatformConfiguration class with properties

--- src/Models/PlatformConfiguration.ps1
+++ src/Models/PlatformConfiguration.ps1
@@ -1,1 +1,15 @@
-#start
+class PlatformConfiguration {
+
+    [string]$PddDirectory
+    [string]$ConfigDirectory
+    [string]$LogDirectory
+    [string]$IniFilesDirectory
+
+    [hashtable]$Toolboxes
+    [string[]]$Catalogs
+
+    PlatformConfiguration() {
+        $this.Toolboxes = @{}
+        $this.Catalogs = @()
+    }
+}
